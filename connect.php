40ad174286aaa2eb5811c89cfaff2a2bad1afb21
NganarembaShija <76985647+NganarembaShija@users.noreply.github.com>
2022-03-13 14:18:25 +0530

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,17 +1,10 @@
 <?php
 
     $host = 'localhost';
-
     $user = 'root';
-
     $pass = '';
-
     $db_name = 'chat';
 
-
-
     $con = mysqli_connect($host,$user,$pass,$db_name) or die('Error connecting to server');
-
-
 
 ?>
